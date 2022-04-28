bbf833e92a8b041083a83dbc6172dd265f6fcf56
[DATA EXPUNGED] ███ <26019675+lwd-temp@users.noreply.github.com>
2022-04-28 20:29:47 +0800

    Upload

---
+++ server/run.bat
@@ -0,0 +1,4 @@
+@echo off
+..\jdk-17.0.1\bin\java.exe -Xms2G -Xmx2G -jar .\paper-1.18.1-152.jar --nogui
+pause
+exit
---
+++ 运行客户端.bat
@@ -0,0 +1,3 @@
+@echo off
+start .\jdk-17.0.1\bin\javaw.exe -jar .\HMCL.jar
+exit
